e2f445d0afdfc78baaf812fcc3507512aeb19236
Tongzhou-Yu <tongzhou.yu@network.rca.ac.uk>
2024-02-22 17:24:06 +0800

    Vision Pro-liked Dome Slider

---
+++ Assets/Scripts/DomeSlider.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    public Slider m_slider; // 指向Slider的引用
+    public Material m_material; // 指向Material的引用
+
+    void Start()
+    {
+        m_material = this.GetComponent<MeshRenderer>().material;
+        // 设置Slider的最小值和最大值
+        m_slider.minValue = -7;
+        m_slider.maxValue = 5;
+
+        // 添加Slider的值改变事件的处理方法
+        m_slider.onValueChanged.AddListener(UpdateMaterialDistance);
+    }
+
+    void UpdateMaterialDistance(float value)
+    {
+        // 更新Material的distance变量
+        m_material.SetFloat("_Distance", value);
+    }
+}
